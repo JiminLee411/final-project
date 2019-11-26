4b12e15986fae4901c2f9924d29e4acdeffa7aad
JiminLee411 <wlals41189@gmail.com>
2019-11-27 00:45:33 +0900

    LJM | [수정] Movie serializer

--- movies/views.py
+++ movies/views.py
@@ -53,6 +53,11 @@ def genres_index(request):
     genre = get_object_or_404(Genre, type=request.GET.get('type'))
     return render(request, 'movies/genre.html', {'genre': genre})
 
+@api_view(["GET"])
+def movies_list(request):
+    movies = Movie.objects.all()
+    serializer = MovieSerializer(movies, many=True)
+    return Response(serializer.data)
 
 @api_view(["GET"])
 def genres_list(request):
@@ -60,87 +65,87 @@ def genres_list(request):
     serializer = GenreSerializer(genres, many=True)
     return Response(serializer.data)
 
-
-@api_view(["GET"])
-def genres_detail(request, pk):
-    genre = get_object_or_404(Genre, pk=pk)
-    serializer = GenreSerializer(genre)
+@api_view(['GET'])
+def actors_list(request):
+    actors = Actor.objects.all()
+    serializer = ActorSerializer(actors, many=True)
     return Response(serializer.data)
 
+# @api_view(["GET"])
+# def genres_detail(request, pk):
+#     genre = get_object_or_404(Genre, pk=pk)
+#     serializer = GenreSerializer(genre)
+#     return Response(serializer.data)
 
 @api_view(["GET"])
-def movies_list(request):
-    movies = Movie.objects.all()
-    serializer = MovieSerializer(movies, many=True)
-    return Response(serializer.data)
-
-
-@api_view(["GET"])
-def movies_detail(request, pk):
-    movie = get_object_or_404(Movie, pk=pk)
+def movies_detail(request, movie_pk):
+    movie = get_object_or_404(Movie, pk=movie_pk)
     serializer = MovieSerializer(movie)
     return Response(serializer.data)
 
+@login_required  
+@api_view(['POST'])
+def rating_create(request, movie_pk):
+    movie = get_object_or_404(Movie, pk= movie_pk)
+    serializer = RatingSerializer(data = request.data)
+    if serializer.is_valid(raise_exception=True):
+        serializer.save(movie_id=movie_pk)
+        return Response({'message': '작성되었습니다'})
 
-@api_view(["GET", "POST"])
-def ratings_list(request, pk):
-    movie = get_object_or_404(Movie, pk=pk)
-    if request.method == "GET":
-        ratings = movie.ratings.all()
-        serializer = RatingSerializer(ratings, many=True)
-        return Response(serializer.data)
+@login_required
+@api_view(['PUT','DELETE'])
+def rating_update_and_delete(request, rating_pk, movie_pk):
+    rating = get_object_or_404(Rating, pk = rating_pk)
+    if request.method == 'PUT':
+        serializer = RatingSerializer(data= request.data, instance=rating)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response({'message': '수정되었습니다'})
     else:
-        if request.user.is_authenticated:
-            request.data["movie"] = movie.id
-            request.data["user"] = request.user.id
-            serializer = RatingSerializer(data=request.data)
-            if serializer.is_valid(raise_exception=True):
-                serializer.save()
-                return Response(serializer.data, status=status.HTTP_201_CREATED)
-        else:
-            return Response(status=status.HTTP_401_UNAUTHORIZED)
-
-
-@api_view(["GET", "DELETE", "PUT"])
-def ratings_detail(request, pk):
-    rating = get_object_or_404(Rating, pk=pk)
-    if request.method == 'GET':
-        serializer = RatingSerializer(rating)
-        return Response(serializer.data)
-    else:
-        if request.user == rating.user:
-            request.data["movie"] = rating.movie.id
-            request.data["user"] = request.user.id
-            if request.method == 'PUT':
-                serializer = RatingSerializer(rating, data=request.data)
-                if serializer.is_valid(raise_exception=True):
-                    serializer.save()
-                    return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
-            elif request.method == 'DELETE':
-                rating.delete()
-                return Response({"message": "삭제되었습니다."}, status=status.HTTP_204_NO_CONTENT)
-        else:
-            return Response(status=status.HTTP_401_UNAUTHORIZED)
-
+        # 작성자와 동일한지 확인해보고 삭제시켜줘
+        score.delete()
+        return Response({'message': '삭제되었습니다.'})
 
 @require_POST
+@login_required
 def rating_create(request, movie_pk):
-    if request.user.is_authenticated:
-        form = RatingForm(request.POST)
-        if form.is_valid():
-            rating = form.save(commit=False)
+    movie = get_object_or_404(Movie, pk=movie_pk)
+    if len(movie.rating_set.filter(user_id = request.user.id)) == 0:
+        rating_form = RatingForm(request.POST)
+        if ratingform.is_valid():
+            rating = rating_form.save(commit=False)
             rating.user = request.user
             rating.movie_id = movie_pk
             rating.save()
-    else:
-        messages.warning(request, '로그인이 필요합니다.')
-    return redirect('movies:movies_detail', movie_pk)
+    return redirect('movies:detail', movie_pk)
 
-def rating_delete(request, movie_pk, rating_pk):
+@login_required
+def rating_update(request, movie_pk, rating_pk):
+    movie = get_object_or_404(Movie, pk=movie_pk)
     rating = get_object_or_404(Rating, pk=rating_pk)
-    rating.delete()
-    messages.warning(request, '리뷰가 삭제되었습니다.')
-    return redirect('movies:movies_detail', movie_pk)
+    if rating.user != request.user:
+        return redirect('movies:detail', movie_pk)
+
+    if request.method == 'POST':
+            ratingform = RatingForm(request.POST, instance=rating)
+            if ratingform.is_valid():
+                rating.save()
+                return redirect('movies:detail', movie_pk)
+    else:
+        ratingform = RatingForm(instance=rating)
+    context = {
+        'ratingform':ratingform
+    }
+    return render(request, 'page/form.html', context)
+
+@login_required
+def rating_delete(request, movie_pk, rating_pk):
+    movie = get_object_or_404(Movie, pk=movie_pk)
+    rating = get_object_or_404(Rating, pk=rating_pk)
+    if request.method == 'POST':
+        rating.delete()
+        messages.warning(request, '리뷰가 삭제되었습니다.')
+    return redirect('movies:detail', movie_pk)
 
 @require_POST
 def like(request, movie_pk):
@@ -152,7 +157,7 @@ def like(request, movie_pk):
             request.user.like_movies.add(movie)
     else:
         messages.warning(request, '로그인이 필요한 기능입니다.')
-    return redirect('movies:movies_detail', movie_pk)
+    return redirect('movies:detail', movie_pk)
 
 def update_score(request, rating_pk):
     rating = get_object_or_404(Rating, pk=rating_pk)
@@ -161,7 +166,7 @@ def update_score(request, rating_pk):
             form = ratingForm(request.POST, instance=rating)
             if form.is_valid():
                 form.save()
-                return redirect('movies:movies_detail', rating.movie.pk)
+                return redirect('movies:detail', rating.movie.pk)
         else:
             form = ratingForm(instance=rating)
         context = {
@@ -170,4 +175,23 @@ def update_score(request, rating_pk):
         return render(request, 'accounts/form.html', context)
     else:
         messages.warning(request, '수정 권한이 없습니다.')
-    return redirect('movies:movies_detail', rating.movie.pk)
+    return redirect('movies:detail', rating.movie.pk)
+
+@login_required
+def rating_update(request, movie_pk, rating_pk):
+    movie = get_object_or_404(Movie, pk=movie_pk)
+    rating = get_object_or_404(Rating, pk=rating_pk)
+    if rating.user != request.user:
+        return redirect('page:list')
+
+    if request.method == 'POST':
+            ratingform = RatingForm(request.POST, instance=rating)
+            if ratingform.is_valid():
+                rating.save()
+                return redirect('page:detail', movie.pk)
+    else:
+        ratingform = RatingForm(instance=rating)
+    context = {
+        'ratingform':ratingform
+    }
+    return render(request, 'page/form.html', context)
